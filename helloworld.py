d9259491f5ef2ee069932666c377c6afa261b4ce
Mittal Makwana <mmakwana@Mittals-Mac-mini.local>
2017-08-04 14:26:01 -0400

    Added Helloworld file

---
+++ helloworld.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+This is My first try to get a hang of the Atom Test editor and learn python3
+
+Following from https://www.tutorialspoint.com/python3/
+
+"""
+print("Hello World")
