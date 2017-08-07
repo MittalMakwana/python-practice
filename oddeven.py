49008b4aac2ab943da7a524eaddb4908d3ca0a79
Mittal Makwana <mmakwana@Mittals-Mac-mini.local>
2017-08-07 12:47:42 -0400

    Added Odd even python test file learned new print python format.

---
+++ oddeven.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# number = input('Enter a number')
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print ("The number %d is Even" % (number))
+else:
+    print ("Number is {0} is ODD" .format(number))
