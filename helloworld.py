dd129c32c74d1f54231679e68f4c7d1289de766b
Mittal Makwana <mmakwana@mittal.local>
2017-08-10 15:39:16 -0400

    Modified file to learn more print function

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python3
-"""
-This is My first try to get a hang of the Atom Test editor and learn python3
 
-Following from https://www.tutorialspoint.com/python3/
-
-"""
-print("Hello World")
+STRING_1 = 'Hello'
+STRING_2 = 'World'
+NUMBER_1 = 3.1456
+NUMBER_2 = 412499.35
+LIST = ['Aldi']
+# LIST = ['Amazon', 'Walmart', 'Gaint Eagle', 'Costco']
+print(STRING_1)
+print(STRING_2)
+print('{} {}' .format(STRING_1, STRING_2))
+print ('{} {}' .format(STRING_1, NUMBER_1))
+print ('This is an way to print pi ' + str(NUMBER_1) + ' With ' + STRING_1)
+print ('This is an other way {:.2f} with {}' .format(NUMBER_1, STRING_1))
+print (NUMBER_1, NUMBER_2)
+print (NUMBER_1 * NUMBER_1)
+print("{} {} on my list".format(
+    len(LIST),
+    "store" if len(LIST) == 1 else "stores",
+))
