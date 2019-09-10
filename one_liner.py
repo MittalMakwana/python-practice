d8e951c66e385e3d97aa72a8aec508bc936c20de
Mittal Makwana <mittalmak@gmail.com>
2019-09-09 20:48:13 -0700

    Update one_liner.py

--- one_liner.py
+++ one_liner.py
@@ -3,3 +3,7 @@ def factorial(num): return reduce(lambda x,y:x*y, (i for i in range(1,num+1)))
 def reverse_str(str): return str[::-1]
 
 def is_pal(str) : return str == str[::-1]
+
+a,b = b,a # swap two variables
+
+def square_list(list): return map(lambda x: x*x, list)
