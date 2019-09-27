ac47930567d51ada758f68e106d66db04e2aeef9
Mittal Makwana <mittalmak@gmail.com>
2019-09-27 15:17:57 -0700

    longest_duplicate in a string
    Given a string S, consider all duplicated substrings: (contiguous) substrings of S that occur 2 or more times.  (The occurrences may overlap.)
    
    Return any duplicated substring that has the longest possible length.  (If S does not have a duplicated substring, the answer is "".)

---
+++ longest_duplicate.py
@@ -0,0 +1,15 @@
+def find_longest_dup(s):
+    hashset = set()
+
+    result = []
+
+    for i in range(len(s)):
+        for j in range(i+1, len(s)+1):
+            if s[i:j] not in hashset:
+                hashset.add(s[i:j])
+            else:
+                result.append(s[i:j])
+    return max(result, key=len) if result else ""
+
+
+print(find_longest_dup("banananabanana"))
